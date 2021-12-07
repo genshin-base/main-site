e115001bc2f794507c6fab4a9bd735260a2a3f48
3bl3gamer <3bl3gamer@gmail.com>
2021-12-08 02:41:00 +0300

    Выпарсивание билдов персонажей из .ods, конфиги линтеров-форматтеров

---
+++ .eslintrc.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+	env: {
+		es6: true,
+		node: true,
+		browser: false,
+	},
+	parserOptions: {
+		sourceType: 'module',
+		ecmaVersion: 'latest',
+	},
+	extends: ['prettier'],
+	rules: {
+		'no-unused-vars': ['warn', { vars: 'all', args: 'none' }],
+		'no-constant-condition': ['error', { checkLoops: false }],
+		eqeqeq: ['warn', 'always'],
+	},
+}
---
+++ generate.js
@@ -0,0 +1,34 @@
+#!/bin/node
+import { promises as fs } from 'fs'
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import { extractBuildsFromODS } from './lib/ods.js'
+import { getFileCached } from './lib/requests.js'
+
+const __filename = fileURLToPath(import.meta.url)
+const baseDir = dirname(__filename)
+const CACHE_DIR = `${baseDir}/cache`
+
+const DOC_ID = '1gNxZ2xab1J6o1TuNVWMeLOZ7TPOqrsf3SshP5DLvKzI'
+
+;(async () => {
+	await fs.mkdir(CACHE_DIR, { recursive: true })
+
+	const docFPath = `${CACHE_DIR}/spreadsheet.odt`
+	const url = `https://docs.google.com/spreadsheets/export?id=${DOC_ID}&exportFormat=ods`
+	await getFileCached(url, null, docFPath, true, Infinity)
+
+	const buildInfo = await extractBuildsFromODS(docFPath)
+	console.log(buildInfo)
+	for (const [elem, info] of Object.entries(buildInfo.elementMap)) {
+		console.log(elem)
+		for (const char of info)
+			console.log(
+				'  ' +
+					(char.name + ': ').padEnd(12) +
+					char.roles.map(x => x.name + (x.isBest ? '+' : '')).join(', '),
+			)
+	}
+
+	// setTimeout(() => {}, 1000000)
+})().catch(console.error)
