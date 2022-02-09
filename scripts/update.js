3c6a51304d8ce96e072e5560e75bdcaff8ec3a52
3bl3gamer <3bl3gamer@gmail.com>
2022-02-09 23:18:01 +0300

    Глубинные вишапы на карте

--- scripts/update.js
+++ scripts/update.js
@@ -209,6 +209,7 @@ const fixes = {
 		enemiesOnMap: [
 			{ nameOnMap: 'Fatui Agent', useCode: 'fatui-pyro-agent' },
 			{ nameOnMap: 'Fatui Mirror Maiden', useCode: 'mirror-maiden' },
+			{ nameOnMap: 'Electro Samachurl', useCode: 'bathysmal-vishap' },
 		],
 	},
 }
