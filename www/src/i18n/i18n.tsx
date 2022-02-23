6470e1fb25120b32a96c396b1454b2fe3bb8e3ef
LookNya <pavelx58@yandex.ru>
2022-02-23 19:23:12 +0300

    Update i18n.tsx

--- www/src/i18n/i18n.tsx
+++ www/src/i18n/i18n.tsx
@@ -9,57 +9,53 @@ import { GI_TalentCode } from '#lib/parsing/helperteam/types'
 
 type Lang = 'en' | 'ru'
 const LANG = BUNDLE_ENV.LANG as Lang
-const l = (en, ru) => {
-	return {
-		en,
-		ru,
-	}
-}
 export const I18N_LANG_NAMES: Record<Lang, string> = {
 	en: 'English',
 	ru: 'Русский',
 }
 export const I18N_LANG_NAME = I18N_LANG_NAMES[LANG]
 
-export const I18N_DASHBOARD = l('Dashboard', 'Самое важное')[LANG]
-export const I18N_REGION = l('Region', 'Регион')[LANG]
-export const I18N_UNTIL_DAY_RESET = l('Until Day Reset', 'До нового дня')[LANG]
-export const I18N_ALCHEMY_CALC = l('Alchemy Calculator', 'Калькулятор алхимии')[LANG]
-export const I18N_WHAT_TO_FARM = l('What to Farm', 'Что фармить')[LANG]
-export const I18N_BUILDS = l('Builds', 'Сборки персонажей')[LANG]
-export const I18N_ASIA = l('Asia', 'Азия')[LANG]
-export const I18N_NORH_AMERICA = l('North America', 'Северная Америка')[LANG]
-export const I18N_EUROPE = l('Europe', 'Европа')[LANG]
-export const I18N_MINUTE = l('minute', 'минута')[LANG]
-export const I18N_MINUTES = l('minutes', 'минуты')[LANG]
-export const I18N_MINUTES_3 = l('minutes', 'минут')[LANG]
-export const I18N_HOUR = l('hour', 'час')[LANG]
-export const I18N_HOURS = l('hours', 'часа')[LANG]
-export const I18N_HOURS_3 = l('hours', 'часов')[LANG]
-export const I18N_TODAY = l('today', 'сегодня')[LANG]
-export const I18N_WEEKDAYS = l(
-	['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
-	['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
-)[LANG]
-export const I18N_TOMORROW = l('tomorrow', 'завтра')[LANG]
-export const I18N_DUNGEONS = l('Dungeons', 'Подземелья')[LANG]
-export const I18N_TALENTS = l('Talents', 'Таланты')[LANG]
-export const I18N_WEAPONS = l('Weapons', 'Оружие')[LANG]
-export const I18N_WHY_ADD_TO_FAVS_TIP = l(
-	'Add characters, weapons or items to your favorites to see them marked here.',
-	'Добавьте персонажей, оружие и предметы в избранные, чтобы отметить их здесь',
-)[LANG]
-export const I18N_FAV_CHARACTERS = l('Favorite characters', 'Избранные персонажи')[LANG]
-export const I18N_ARTIFACTS = l('Artifacts', 'Артефакты')[LANG]
-export const I18N_ART_STATS_PRIORITY = l('Artifact stats priority', 'Приоритетные главстаты')[LANG]
-export const I18N_SUBSTATS_PRIORITY = l('Substats priority', 'Приоритетные сабстаты')[LANG]
-export const I18N_TALENTS_PRIORITY = l('Talents Priority', 'Приоритетные умения')[LANG]
-export const I18N_ASC_MATERIALS = l('Ascension materials', 'Материалы возвышения')[LANG]
-export const I18N_FULL_BUILD_INFO = l('Full build info', 'Полная информация о билде')[LANG]
-export const I18N_CHAR_LORE = l('Character lore', 'Лор персонажа')[LANG]
-export const I18N_MORE_ON_BUILDS_PAGE = l('more on build page', 'продолжение на странице билда')[LANG]
-export const I18N_BACK = l('Back', 'Назад')[LANG]
-export const I18N_NOTES = l('Notes', 'Примечания')[LANG]
+export const I18N_DASHBOARD = { en: 'Dashboard', ru: 'Самое важное' }[LANG]
+export const I18N_REGION = { en: 'Region', ru: 'Регион' }[LANG]
+export const I18N_UNTIL_DAY_RESET = { en: 'Until Day Reset', ru: 'До нового дня' }[LANG]
+export const I18N_ALCHEMY_CALC = { en: 'Alchemy Calculator', ru: 'Калькулятор алхимии' }[LANG]
+export const I18N_WHAT_TO_FARM = { en: 'What to Farm', ru: 'Что фармить' }[LANG]
+export const I18N_BUILDS = { en: 'Builds', ru: 'Сборки персонажей' }[LANG]
+export const I18N_ASIA = { en: 'Asia', ru: 'Азия' }[LANG]
+export const I18N_NORH_AMERICA = { en: 'North America', ru: 'Северная Америка' }[LANG]
+export const I18N_EUROPE = { en: 'Europe', ru: 'Европа' }[LANG]
+export const I18N_MINUTE = { en: 'minute', ru: 'минута' }[LANG]
+export const I18N_MINUTES = { en: 'minutes', ru: 'минуты' }[LANG]
+export const I18N_MINUTES_3 = { en: 'minutes', ru: 'минут' }[LANG]
+export const I18N_HOUR = { en: 'hour', ru: 'час' }[LANG]
+export const I18N_HOURS = { en: 'hours', ru: 'часа' }[LANG]
+export const I18N_HOURS_3 = { en: 'hours', ru: 'часов' }[LANG]
+export const I18N_TODAY = { en: 'today', ru: 'сегодня' }[LANG]
+export const I18N_WEEKDAYS = {
+	en: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+	ru: ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
+}[LANG]
+export const I18N_TOMORROW = { en: 'tomorrow', ru: 'завтра' }[LANG]
+export const I18N_DUNGEONS = { en: 'Dungeons', ru: 'Подземелья' }[LANG]
+export const I18N_TALENTS = { en: 'Talents', ru: 'Таланты' }[LANG]
+export const I18N_WEAPONS = { en: 'Weapons', ru: 'Оружие' }[LANG]
+export const I18N_WHY_ADD_TO_FAVS_TIP = {
+	en: 'Add characters, weapons or items to your favorites to see them marked here.',
+	ru: 'Добавьте персонажей, оружие и предметы в избранные, чтобы отметить их здесь',
+}[LANG]
+export const I18N_FAV_CHARACTERS = { en: 'Favorite characters', ru: 'Избранные персонажи' }[LANG]
+export const I18N_ARTIFACTS = { en: 'Artifacts', ru: 'Артефакты' }[LANG]
+export const I18N_ART_STATS_PRIORITY = { en: 'Artifact stats priority', ru: 'Приоритетные главстаты' }[LANG]
+export const I18N_SUBSTATS_PRIORITY = { en: 'Substats priority', ru: 'Приоритетные сабстаты' }[LANG]
+export const I18N_TALENTS_PRIORITY = { en: 'Talents Priority', ru: 'Приоритетные умения' }[LANG]
+export const I18N_ASC_MATERIALS = { en: 'Ascension materials', ru: 'Материалы возвышения' }[LANG]
+export const I18N_FULL_BUILD_INFO = { en: 'Full build info', ru: 'Полная информация о билде' }[LANG]
+export const I18N_CHAR_LORE = { en: 'Character lore', ru: 'Лор персонажа' }[LANG]
+export const I18N_MORE_ON_BUILDS_PAGE = { en: 'more on build page', ru: 'продолжение на странице билда' }[
+	LANG
+]
+export const I18N_BACK = { en: 'Back', ru: 'Назад' }[LANG]
+export const I18N_NOTES = { en: 'Notes', ru: 'Примечания' }[LANG]
 
 const artTypeNamesRU: Record<GI_ArtifactTypeCode, string> = {
 	flower: 'цветок',
@@ -128,41 +124,42 @@ export const I18N_CONJUCTIONS: Record<ConjuctionType, string> = {
 	ru: { or: 'или', and: 'и' },
 }[LANG]
 
-export const I18N_RECOMENDED_FOR = l('Recommended for', 'Рекомендуется для')[LANG]
-export const I18N_FOR_NOBODY = l('Nobody', 'Никому')[LANG]
-export const I18N_SOURCE = l('Source', 'Источник')[LANG]
-export const I18N_SCROLL_TO_ZOOM = l('Scroll to zoom', 'Зум колесиком')[LANG]
-export const I18N_PINCH_TO_ZOOM = l('Pinch to zoom', 'Зум щипком')[LANG]
-export const I18N_ERROR = l('Error, reload page', 'Ошибка, перезагрузите страницу')[LANG]
-export const I18N_PIECE_BONUS = l(
-	(n: number) => `${n} piece bonus`,
-	(n: number) => `Бонус ${n} части`,
-)[LANG]
-export const I18N_PIECES_BONUS = l(
-	(n: number) => `${n} pieces bonus`,
-	(n: number) => `Бонус ${n} частей`,
-)[LANG]
-export const I18N_BASE_ATTACK = l('Base attack', 'Базовая атака')[LANG]
-export const I18N_SECONDARY_STAT = l('Secondary Stat', 'Пассивная способность')[LANG]
+export const I18N_RECOMENDED_FOR = { en: 'Recommended for', ru: 'Рекомендуется для' }[LANG]
+export const I18N_FOR_NOBODY = { en: 'Nobody', ru: 'Никому' }[LANG]
+export const I18N_SOURCE = { en: 'Source', ru: 'Источник' }[LANG]
+export const I18N_SCROLL_TO_ZOOM = { en: 'Scroll to zoom', ru: 'Зум колесиком' }[LANG]
+export const I18N_PINCH_TO_ZOOM = { en: 'Pinch to zoom', ru: 'Зум щипком' }[LANG]
+export const I18N_ERROR = { en: 'Error, reload page', ru: 'Ошибка, перезагрузите страницу' }[LANG]
+export const I18N_PIECE_BONUS = {
+	en: (n: number) => `${n} piece bonus`,
+	ru: (n: number) => `Бонус ${n} части`,
+}[LANG]
+export const I18N_PIECES_BONUS = {
+	en: (n: number) => `${n} pieces bonus`,
+	ru: (n: number) => `Бонус ${n} частей`,
+}[LANG]
+export const I18N_BASE_ATTACK = { en: 'Base attack', ru: 'Базовая атака' }[LANG]
+export const I18N_SECONDARY_STAT = { en: 'Secondary Stat', ru: 'Пассивная способность' }[LANG]
 type MapCodeName = Record<MapCode, string>
-export const I18N_MAP_CODES_NAME: MapCodeName = l(
-	{ teyvat: 'Teyvat', enkanomiya: 'Enkanomiya' },
-	{ teyvat: 'Тейват', enkanomiya: 'Энканомия' },
-)[LANG]
-export const I18N_CHAR_BUILD_RECS = l('Character builds recomendations', 'Рекомендуемые сборки персонажей')[
-	LANG
-]
-export const I18N_SELECT_CHAR_ABOVE = l('Select character above', 'Выберите персонажа')[LANG]
-export const I18N_BUILD_RECS_FOR = l('build recomendations', 'Рекомендованный билд для')[LANG]
-export const I18N_BASED_ON_GIHT = l(
-	"Based on Genshin Impact Helper Team's Character Builds",
-	'Основано на табличке Геншин Импакт Хелпер Тимы (англ.)',
-)[LANG]
-export const I18N_ABOUT_SITE = l(`About`, 'О сайте')[LANG]
-export const I18N_CREATED_BY_US = l(
-	'Designed and coded by Absolute Evil Studio',
-	'Задизайнено и закожено в Абсолют Ивел студии',
-)[LANG]
+export const I18N_MAP_CODES_NAME: MapCodeName = {
+	en: { teyvat: 'Teyvat', enkanomiya: 'Enkanomiya' },
+	ru: { teyvat: 'Тейват', enkanomiya: 'Энканомия' },
+}[LANG]
+export const I18N_CHAR_BUILD_RECS = {
+	en: 'Character builds recomendations',
+	ru: 'Рекомендуемые сборки персонажей',
+}[LANG]
+export const I18N_SELECT_CHAR_ABOVE = { en: 'Select character above', ru: 'Выберите персонажа' }[LANG]
+export const I18N_BUILD_RECS_FOR = { en: 'build recomendations', ru: 'Рекомендованный билд для' }[LANG]
+export const I18N_BASED_ON_GIHT = {
+	en: "Based on Genshin Impact Helper Team's Character Builds",
+	ru: 'Основано на табличке Геншин Импакт Хелпер Тимы (англ.)',
+}[LANG]
+export const I18N_ABOUT_SITE = { en: 'About', ru: 'О сайте' }[LANG]
+export const I18N_CREATED_BY_US = {
+	en: 'Designed and coded by Absolute Evil Studio',
+	ru: 'Задизайнено и закожено в Абсолют Ивел студии',
+}[LANG]
 
 type WeaponTypeNames = Record<GI_WeaponTypeCode, string>
 const weaponTypeNamesRU: WeaponTypeNames = {
@@ -198,7 +195,7 @@ export const I18N_WEAPON_OBTAIN_SOURCE_NAME = {
 	en: (code: GI_WeaponObtainSource) => code.replace(/-/g, ' '),
 	ru: (code: GI_WeaponObtainSource) => weaponObtainSourceNamesRU[code],
 }[LANG]
-export const I18N_OBTAINED_DURING_STORYLINE = l(
-	'Obtained during storyline quests',
-	'Выдаётся во время прохождения сюжетных заданий',
-)[LANG]
+export const I18N_OBTAINED_DURING_STORYLINE = {
+	en: 'Obtained during storyline quests',
+	ru: 'Выдаётся во время прохождения сюжетных заданий',
+}[LANG]
