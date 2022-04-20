bd0d8aa461d2b63011af46f9c08d93d3d3bc57aa
Looknya <pavelx58@yandex.ru>
2022-04-20 14:32:16 +0300

    Update mega-search.tsx

--- www/src/containers/mega-search.tsx
+++ www/src/containers/mega-search.tsx
@@ -1,39 +1,3 @@
-// import { useCallback, useMemo, useState } from 'preact/hooks'
-
-// import { apiGetSearchData } from '#src/api/endpoints'
-// import { isLoaded, useFetch } from '#src/utils/hooks'
-
-// import './mega-search.scss'
-
-// export function MegaSearch({ classes = '' }: { classes?: string }): JSX.Element {
-// 	const searchData = useFetch(apiGetSearchData, [])
-// 	const [searchStr, setSearchStr] = useState<string>('')
-
-// 	const searchResult = useMemo(() => {
-// 		if (!isLoaded(searchData)) return searchData
-// 		const strLC = searchStr.trim().toLocaleLowerCase()
-// 		if (searchStr === '') return []
-// 		return searchData.filter(
-// 			x => x.name.toLocaleLowerCase().includes(strLC) || x.nameEn?.toLocaleLowerCase().includes(strLC),
-// 		)
-// 	}, [searchData, searchStr])
-
-// 	const onSearchValueChange = useCallback(e => setSearchStr(e.target.value), [setSearchStr])
-// 	return (
-// 		<div className={`mega-search ${classes}`}>
-// 			<input type="search" value={searchStr} onInput={onSearchValueChange} />
-// 			<div className="search-results-dd">
-// 				{isLoaded(searchResult)
-// 					? searchResult.map(x => (
-// 							<div>
-// 								{x.type}:{x.code}
-// 							</div>
-// 					  ))
-// 					: 'луадинг...'}
-// 			</div>
-// 		</div>
-// 	)
-// }
 import { SearchItem } from '#src/../../lib/parsing/combine'
 import { apiGetSearchData } from '#src/api/endpoints'
 import { CentredSpinner } from '#src/components/placeholders'
