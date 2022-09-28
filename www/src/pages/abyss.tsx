5186288e04d11b054e2de6ad2d061f8a7f8868f8
Looknya <pavelx58@yandex.ru>
2022-09-28 19:43:36 +0300

    фиксы страницы бездны

--- www/src/pages/abyss.tsx
+++ www/src/pages/abyss.tsx
@@ -9,10 +9,10 @@ export function AbyssPage(): JSX.Element {
 	useDocumentTitle('статистика бездны' + I18N_PAGE_TITLE_POSTFIX)
 	return (
 		<div className="abyss container">
-			<div class="position-relative overflow-hidden rounded-3 py-4 px-5 my-4 please-tile">
+			<div class="position-relative overflow-hidden rounded-3 py-4 px-md-5 px-4 my-4 please">
 				<div className="please-bg"></div>
-				<p className="fs-3 text-center">
-					Please help us to collect data about characters usage in Abyss
+				<p className="fs-3 text-center please-text">
+					Help us to collect data about characters usage in Abyss
 				</p>
 				<p className="fs-5 text-center">
 					Just leave your UID here and check if your battle history public
@@ -25,6 +25,9 @@ export function AbyssPage(): JSX.Element {
 					<button className="btn btn-danger btn-lg ms-2">Submit</button>
 				</div>
 				<div className="mt-4 mb-3 px-2">
+					<div className="text-center mb-2 please-text">
+						<label>Please share this page, so we can faster finish data collection.</label>
+					</div>
 					<div class="progress ">
 						<div
 							class="progress-bar"
@@ -36,7 +39,7 @@ export function AbyssPage(): JSX.Element {
 							240 UIDs from 1000 collected
 						</div>
 					</div>
-					<div className="text-center mt-4 please-follow">
+					<div className="text-center mt-4 please-text">
 						<label>
 							If you want to know when statistics will be ready, join{' '}
 							<a href="#" className="link-danger">
@@ -51,16 +54,18 @@ export function AbyssPage(): JSX.Element {
 				</div>
 			</div>
 			<h1 className="my-1 letter-spacing-1">{'Abyss statistics'}</h1>
+			<label className="text-muted">This is sample data: actual for v2.7</label>
 			<div className="row">
-				<div className="col-3">
+				<div className="col-12 col-lg-3">
 					<BlockHeader classes="mt-3">{'Most used characters'}</BlockHeader>
 					<CharListWithUsage classes="col-s-content" />
 				</div>
-				<div className="col-9">
+				<div className="col-12 col-lg-9">
 					<BlockHeader classes="mt-3">{'History of character usage'}</BlockHeader>
 					<div className="position-relative col-e-content">
-						<img style={{ size: 'cover' }} src="https://i.imgur.com/j1gVE7h.png"></img>
-						<div className="position-absolute top-0 bottom-0 start-0 end-0 d-flex justify-content-center align-items-center">
+						{/* <img style={{ size: 'cover' }} src="https://i.imgur.com/j1gVE7h.png"></img> */}
+						<iframe style={{ height: '100%', width: '100%' }} src="/c/index.html"></iframe>
+						<div className="pe-none position-absolute top-0 bottom-0 start-0 end-0 d-flex justify-content-center align-items-center">
 							{'example of data'}
 						</div>
 					</div>
